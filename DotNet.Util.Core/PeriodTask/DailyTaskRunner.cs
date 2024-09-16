7d1f75c030950dbab93979dae302dffce9e4b791
辛京爵 <1578814458@qq.com>
2024-09-16 21:13:05 +0800

    修改命名空间

--- DotNet.Util.Core/PeriodTask/DailyTaskRunner.cs
+++ DotNet.Util.Core/PeriodTask/DailyTaskRunner.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DotNet.Util.Core.PeriodTask
+namespace Xin.DotnetUtil.PeriodTask
 {
     public class DailyTaskRunner
     {
